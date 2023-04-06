7c4e55b1d46176fdb212fe9a84d58407008af380
Raan_at_Git (Windows) <74211378+Raan-Saurav-Bhuyan@users.noreply.github.com>
2023-04-06 10:53:29 +0530

    Introduce Variables in C.
    Added a source file to debut the numeric and character variables in C and how to print them on the output screen.

---
+++ Introduction/variables.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    /**
+     * @brief Different types of numeric and character variables are assigned values.
+     *
+     * @version 0.1
+     * @author Raan_at_Git (Windows) (74211378+Raan-Saurav-Bhuyan@users.noreply.github.com)
+     * @date 06-04-2023
+     * @copyright Copyright (c) 2023
+     */
+    int x = 10, y = 20;
+    float p = 0.11, q = 0.22;
+    char a = 'a', b = 'b';
+
+    /**
+     * @brief Print the variables in order and give their values to the output stream.
+     *
+     * @version 0.1
+     * @author Raan_at_Git (Windows) (74211378+Raan-Saurav-Bhuyan@users.noreply.github.com)
+     * @date 06-04-2023
+     * @copyright Copyright (c) 2023
+     */
+    printf("x = %d\n", x);
+    printf("y = %d\n", y);
+    printf("p = %f\n", p);
+    printf("q = %f\n", q);
+    printf("a = %c\n", a);
+    printf("b = %c\n", b);
+}
