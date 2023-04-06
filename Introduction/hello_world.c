a15786c317616325d9c8829382267e35a9703f78
Raan_at_Git (Windows) <74211378+Raan-Saurav-Bhuyan@users.noreply.github.com>
2023-04-06 10:40:28 +0530

    Add Introductory Source File.
    Added an introductory C source file to make
    1. the syntax and basic functions (such as main() and getch()),
    2. header files etc.
    familiar with the learner.

---
+++ Introduction/hello_world.c
@@ -0,0 +1,39 @@
+/**
+ * @file hello_world.c
+ * @author Raan_at_Git (Windows) (74211378+Raan-Saurav-Bhuyan@users.noreply.github.com)
+ * @brief First C program source code with introduction to basic structure or syntax.
+ * @version 0.1
+ * @date 06-04-2023
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+// Pre-processor directives
+#include <stdio.h>
+#include <conio.h>
+
+/**
+ * @brief Definitions + declarations for the main function
+ *
+ * @return int
+ * @version 0.1
+ * @author Raan_at_Git (Windows) (74211378+Raan-Saurav-Bhuyan@users.noreply.github.com)
+ * @date 06-04-2023
+ * @copyright Copyright (c) 2023
+ */
+int main()
+{
+  printf("Hello, world!\nWelcome to C Tutorials.\n");
+  printf("This source file is compiled with the LLVM toolchain.\n");
+
+  /**
+   * @brief Construct a new getch object, Function call to hold the execution window or prompt
+   *
+   * @version 0.1
+   * @author Raan_at_Git (Windows) (74211378+Raan-Saurav-Bhuyan@users.noreply.github.com)
+   * @date 06-04-2023
+   * @copyright Copyright (c) 2023
+   */
+  getch();
+}
