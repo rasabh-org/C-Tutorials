73cb02a1e65e85371349b39bcfc4ad4bf630a443
Raan_at_Git (Windows) <74211378+Raan-Saurav-Bhuyan@users.noreply.github.com>
2023-04-06 11:18:51 +0530

    Introduce Data Types in C.
    Added a C source file to demonstrate the usage of primitive data types in C.

---
+++ Introduction/data_types.c
@@ -0,0 +1,48 @@
+/**
+ * @file data_types.c
+ * @author Raan_at_Git (Windows) (74211378+Raan-Saurav-Bhuyan@users.noreply.github.com)
+ * @brief Introduction to data types in C and how to use them.
+ * @version 0.1
+ * @date 06-04-2023
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <stdio.h>
+#include <conio.h>
+// To use bool data types.
+#include <stdbool.h>
+
+int main()
+{
+    /**
+     * @brief Declaration of variables with different primitive data types
+     * and assign values to them.
+     *
+     * @version 0.1
+     * @author Raan_at_Git (Windows) (74211378+Raan-Saurav-Bhuyan@users.noreply.github.com)
+     * @date 06-04-2023
+     * @copyright Copyright (c) 2023
+     */
+    char a = 'a';
+    int b = 100;
+    float c = 500.50;
+    double d = 15.154245;
+    bool e = true;
+    bool f = false;
+
+    /**
+     * @brief Printf() function to print the values of different data types.
+     *
+     * @version 0.1
+     * @author Raan_at_Git (Windows) (74211378+Raan-Saurav-Bhuyan@users.noreply.github.com)
+     * @date 06-04-2023
+     * @copyright Copyright (c) 2023
+     */
+    printf("a = %c\n", a);
+    printf("b = %d\n", b);
+    printf("c = %f\n", c);
+    printf("d = %f\n", d);
+    printf("e = %d\n", e);
+    printf("f = %d\n", f);
+}
